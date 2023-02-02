fbc986aadc05dbf2f2a63a333c8b67e9ca349675
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-02-02 18:09:24 +0300

    Add modified js files by adding the remove test function

--- addRemoveItems.test.js
+++ addRemoveItems.test.js
@@ -4,22 +4,19 @@
 
 import { ItemSaved } from './src/modules/addRemoveItems.js';
 
-test('use jsdom in this test file', () => {
-  const element = document.createElement('div');
-  expect(element).not.toBeNull();
-});
-
 describe('ItemSaved class', () => {
-//   beforeEach(() => {
-//     const list = [];
-//     list.length = 0;
-//   });
-
   test('adds a new item to the list', () => {
     const item = new ItemSaved();
     item.newItem('Test item', false);
     const list = JSON.parse(localStorage.getItem('list')) || [];
     expect(list).toHaveLength(1);
-    // expect(list[0].description).toEqual('Test item');
+  });
+
+  test('removes an item from the list', () => {
+    const item = new ItemSaved();
+    item.newItem('Test item', false);
+    item.removeTask(2, false);
+    const list = JSON.parse(localStorage.getItem('list')) || [];
+    expect(list).toHaveLength(1);
   });
 });
